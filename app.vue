0cc15a91b594c2fb974dce52ab2bb2c54b7b8b50
Your Name <you@example.com>
2025-09-05 15:22:54 +0900

    aaaaa

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <div class="max-w-max mx-auto border border-gray-200 rounded-lg px-6 py-3 bg-white shadow-sm"></div>
+        <div class="max-w-max mx-auto border border-gray-200 rounded-lg px-6 py-3 bg-white shadow-sm">
         <h1 class="text-3xl font-bold text-center">Hello world</h1>
         </div>
     </header>

1c1bdc72e7a87eee9a86b3ed8bda9d9dee429814
Your Name <you@example.com>
2025-09-05 15:27:12 +0900

    aaaaa

--- app.vue
+++ app.vue
@@ -1,8 +1,6 @@
 <template>
-    <header>
-        <div class="max-w-max mx-auto border border-blue-200 rounded-lg px-6 py-3 bg-white shadow-sm">
-        <h1 class="text-3xl font-bold text-center">Hello world</h1>
-        </div>
+    <header class="bg-blue-500 p-4">
+        <h1 class="text-3xl font-bold text-center text-white">Hello world</h1>
     </header>
     <NuxtPage />
 </template>

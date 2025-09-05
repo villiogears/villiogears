6beac4805eb3dfcb9bbcf12b5c772a2394bf1474
Your Name <you@example.com>
2025-09-06 00:46:40 +0900

    aaaaa

--- components/BG.vue
+++ components/BG.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-        
+        <img src="/bouzaidan.svg" alt="" aria-hidden="true" loading="eager"
+            style="position:fixed; top:50%; left:50%; transform:translate(-50%,-50%); width:140vw; height:auto; z-index:0; pointer-events:none; user-select:none;" />
     </div>
 </template>

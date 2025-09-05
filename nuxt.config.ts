672d0caaae5c166b00c85e32b0618182165f9be9
Your Name <you@example.com>
2025-09-06 08:03:00 +0900

    aaaaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'NSR elphadeal',
+      titleTemplate: '%s - 磁石祭ZERO',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'A Nuxt 3 PWA' }
+      ]
+    }
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
